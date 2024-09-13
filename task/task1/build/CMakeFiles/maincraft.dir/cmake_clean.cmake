211c471ab67c4215c33200109bdfc3cc61474015
LidiaReb <81241178+LidiaReb@users.noreply.github.com>
2024-09-14 00:28:55 +0300

    d

---
+++ task/task1/build/CMakeFiles/maincraft.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maincraft.dir/main.cpp.o"
+  "CMakeFiles/maincraft.dir/main.cpp.o.d"
+  "maincraft"
+  "maincraft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maincraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
